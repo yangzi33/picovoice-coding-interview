06c8f87736063794ef3c6722825fc498c38534fe
Ziyue Yang <ziyue.yang@mail.utoronto.ca>
2020-08-28 13:47:24 -0700

    Initial commit

---
+++ python-regex-matching/match_regex.py
@@ -0,0 +1,26 @@
+# Matching regex
+class Solve:
+    def match_regex(self, s: str, p: str) -> bool:
+        if not p:
+            return not s
+
+        # Check first occurence of string
+        # Base case for recursive algorithm
+        first_check = not not s and (s[0] == p[0] or p[0] == '.')
+        
+        # if not first:
+        #     return False
+        
+        # Recursion if length of pattern >= 2
+        if len(p) > 1:
+            # Check Kleene Star
+            if p[1] == '*':
+                # Condition 1: perform recursion till 'first' is false
+                return (first_check and self.match_regex(s[1:], p)) or self.match_regex(s, p[2:])  
+        return self.match_regex(s[1:], p[1:]) and first_check
+
+
+if __name__ == '__main__':
+    # pass
+    solve = Solve()
+    print(solve.match_regex('a', 'a*'))
